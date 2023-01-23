b362b1015f2f82b31b32a1366a8d8e51804cbc40
makerMKl <115981917+makerMKl@users.noreply.github.com>
2023-01-23 16:13:02 +0000

    base code
    prj

---
+++ src/mainBody.java
@@ -0,0 +1,8 @@
+
+
+public class mainBody {
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.println("Starting >>>>>>>>>>>>>>>><<<<<<<<<<<<<<<System");
+        
+    }
+}
